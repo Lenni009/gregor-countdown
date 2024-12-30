dfaa545d9d0327b84b52ba275860611a5f3b7d4d
Lenni <guentherhanslphdyt@gmail.com>
2024-12-30 13:48:26 +0100

    Update timestamp.ts

--- src/timestamp.ts
+++ src/timestamp.ts
@@ -1,2 +1,2 @@
 // https://www.unixtimestamp.com/
-export const meetingTimestamp = 1723816800000; // Friday, 16 August 2024, 12:00pm
+export const meetingTimestamp = 1705046400000; // Sunday, 12 January 2025, 09:00am
